9fb7cd40d7669069871d526609cd57985fc2f896
Raphael Stefanini <raphael@rphlo.com>
2023-09-04 09:44:10 +0300

    text

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require("./api");
 const port = 3526;
 
 app.listen(port, () => {
-    console.log(`App listening on http://[::]:${port}`)
+    console.log(`App running on http://[::]:${port}`)
 })
